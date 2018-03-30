cc09baaeaa1989f1eb735c2da690aaa118d34bfd
vohungvi <vohungvi@vohungvi.com>
2018-03-30 11:10:05 +0700

    remove unused function

--- TGMTtemplate/Example.cpp
+++ TGMTtemplate/Example.cpp
@@ -1,83 +1,9 @@
 #include "Example.h"
 
-Example* Example::instance = nullptr;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-Example::Example()
+cv::Mat Example::LoadImage(std::string filePath)
 {
-}
+	cv::Mat mat = cv::imread(filePath);
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-Example::~Example()
-{
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-bool Example::DetectStar(std::string filePath)
-{
-	m_totalStar = 0;
-	m_totalContour = 0;
-
-	cv::Mat im = cv::imread(filePath, CV_LOAD_IMAGE_COLOR);
-
-	if (!im.data)
-		return false;
-
-	cv::Mat imgrey = im.clone();
-	cv::cvtColor(im, imgrey, CV_RGB2GRAY);
-	std::vector<std::vector<cv::Point> > contours;
-	std::vector<cv::Vec4i> hierarchy;
-
-	double divMaxSize = 0.175, divMinSize = 0.125;
-
-#ifdef _DEBUG
-	//cv::namedWindow("Image", CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED);
-#endif
-
-	cv::threshold(imgrey, imgrey, 200, 255, 0);
-
-	cv::findContours(imgrey, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
-
-	m_totalContour = contours.size();
-	for (int i = 0; i < m_totalContour; i++)
-	{
-		std::vector<cv::Point> contour = contours[i];
-
-		std::cout << "sqrt(Area)/arcLength = " << sqrt(cv::contourArea(contour)) / cv::arcLength(contour, true) << std::endl;
-		if (sqrt(cv::contourArea(contour)) / cv::arcLength(contour, true) < divMaxSize && sqrt(cv::contourArea(contour)) / arcLength(contour, true) > divMinSize)
-		{
-			cv::drawContours(im, contours, i, RED, 1, 4);
-			m_totalStar++;
-		}
-#ifdef _DEBUG
-		//cv::imshow("Image", im);
-#endif
-	}
-	m_mat = im;
-
-	return true;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-cv::Mat Example::GetImage()
-{
-	return m_mat;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-int Example::GetTotalStar()
-{
-	return m_totalStar;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-int Example::GetTotalContour()
-{
-	return m_totalContour;
+	cv::putText(mat, "thigiacmaytinh.com", cv::Point(0, 35), 1, cv::FONT_HERSHEY_DUPLEX, cv::Scalar(0,0, 255), 2);
+	return mat;
 }
